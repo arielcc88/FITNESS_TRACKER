6525a3b0c8272b0c413795f8f552f1c918a4b2c9
Ariel Cuesta <arielcc88@gmail.com>
2020-09-15 23:26:47 -0500

    adding connection to atlas DB in Heroku.

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,11 +1,13 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
 
 let workoutSeed = [
   {
